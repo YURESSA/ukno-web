33cd9c4321399dee5ff6f90e3667eb89a1ea4f72
Anton787 <antongagelganc6443@gmail.com>
2025-04-28 21:13:38 +0500

    init vue-project + main page + filter

---
+++ frontend/src/components/UI/news-card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+  class="card"
+  :style="{ backgroundImage: `url(/img/${imageName})` }"
+  >
+    <p>{{ title }}</p>
+    <RouterLink to="">
+      узнать подробнее<img src="/icon/white-arrow.svg" alt="">
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  imageName: String,
+  title: String,
+})
+</script>
+
+<style scoped>
+.card{
+  height: 180px;
+  display: flex;
+  justify-content: space-between;
+  background-size: 100%;
+  /* background-repeat: repeat; */
+  /* либо contain & round */
+  align-items: end;
+  color: white;
+  padding: 20px;
+}
+
+a{
+  color: white;
+}
+
+img{
+  margin-left: 10px;
+}
+</style>
