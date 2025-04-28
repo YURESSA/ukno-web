bec9a68d91b53452366405a85204ae5940b2f6c2
Anton787 <antongagelganc6443@gmail.com>
2025-04-28 21:13:38 +0500

    init vue-project + main page + filter

---
+++ frontend/src/components/main/news-block.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="title" id="news">
+    <h3>Новости молодежного бюро</h3>
+  </div>
+  <div class="news-content">
+    <NewsCard title="ПРОГРЕСС-24" imageName="news-img.png"></NewsCard>
+    <NewsCard title="ПРОГРЕСС-24" imageName="news-img4.png"></NewsCard>
+    <div class="half-width-container">
+      <NewsCard title="ПРОГРЕСС-24" imageName="news-img3.png" class="half-width"></NewsCard>
+      <NewsCard title="ПРОГРЕСС-24" imageName="news-img2.png" class="half-width"></NewsCard>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import NewsCard from '../UI/news-card.vue';
+</script>
+
+<style scoped>
+.news-content {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin-bottom: 90px;
+  margin-top: 40px;
+}
+
+.half-width-container {
+  display: flex;
+  gap: 30px;
+}
+
+.half-width {
+  flex: 1;
+}
+</style>
---
+++ frontend/src/components/main/us-result.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="result-wrapper">
+    <div class="title">
+      <h3>Наш результат — ваше вдохновение</h3>
+    </div>
+    <div class="result-list">
+      <ul>
+        <li>
+          <div class="content solo orange">
+            <p class="large-text">Выставки</p>
+          </div>
+        </li>
+        <li>
+          <div class="content">
+            <p class="large-text">Развитие</p>
+          </div>
+          <div class="content">
+            <p class="large-text">240 м² возможностей</p>
+          </div>
+        </li>
+        <li>
+          <div class="content">
+            <p class="large-text">Сообщество мечтателей</p>
+          </div>
+          <div class="content">
+            <p class="large-text">Арт-пространство для всех</p>
+          </div>
+        </li>
+        <li>
+          <div class="content">
+            <p class="large-text">История с 1978 года</p>
+          </div>
+          <div class="content">
+            <p class="large-text">Творчество без границ</p>
+          </div>
+          <div class="content">
+            <p class="large-text">Новое в старом</p>
+          </div>
+        </li>
+        <li>
+          <div class="content">
+            <p class="large-text">Мастер-классы будущего</p>
+          </div>
+          <div class="content">
+            <p class="large-text">Экскурсии в прошлое</p>
+          </div>
+          <div class="content custom">
+            <p class="large-text">90 м² уюта</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.result-wrapper{
+  background-color: white;
+  border-radius: 38px 38px 0px 0px;
+  padding: 80px 50px;
+}
+
+h3{
+  text-align: center;
+}
+
+ul{
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+}
+
+li{
+  display: flex;
+  gap: 10px;
+}
+
+.result-list{
+  margin-top: 80px;
+}
+
+.content{
+  display: flex;
+  justify-content: center;
+  padding: 20px 40px;
+  border: 2px solid #333333;
+  border-radius: 30px;
+}
+
+
+.solo{
+  width: 220px;
+}
+
+.orange{
+  color: #F25C03;
+  border-color: #F25C03;
+}
+
+.custom{
+  width: 280px;
+}
+</style>
