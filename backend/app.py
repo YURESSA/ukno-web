03d7f079862d25b96eaaae02fda6ffb6684d88fb
YURESSA <goshrenko@gmail.com>
2025-05-10 23:41:25 +0500

    Добавлено создание админа

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
-from backend.core import create_app
-
+from backend.core import create_app, db
+from backend.core.models.auth_models import Role
+from backend.core.scripts.role_initializer import ensure_roles_exist
 
 if __name__ == '__main__':
     app = create_app()
+    with app.app_context():
+        ensure_roles_exist(db, Role)
     app.run(debug=True, use_reloader=True)
