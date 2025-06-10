dca046f44a5fb8202e87f4f399da371cf3a8545c
YURESSA <goshrenko@gmail.com>
2025-06-10 15:29:35 +0500

    Тест

--- frontend/src/stores/counter.js
+++ frontend/src/stores/counter.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
-export const baseUrl = 'http://127.0.0.1:5000/'
+export const baseUrl = ''
 
 export const useDataStore = defineStore('data', {
   state: () => ({
