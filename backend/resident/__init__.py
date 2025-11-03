72879c2033932a32fa5bb226ebe658f59e85628e
YURESSA <goshrenko@gmail.com>
2025-11-03 15:39:57 +0500

    Добавлен игнор для предупреждений линтера в импортах в __init__.py

--- backend/resident/__init__.py
+++ backend/resident/__init__.py
@@ -2,5 +2,5 @@ from flask_restx import Namespace
 
 resident_ns = Namespace('resident', description='Эндпоинты для резидента')
 
-from . import (resident_auth, resident_analytics,
+from . import (resident_auth, resident_analytics, # noqa: F401, E402
                resident_sessions, resident_excursions, resident_photos)  # noqa: F401, E402
