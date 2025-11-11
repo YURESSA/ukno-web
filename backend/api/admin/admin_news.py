da8b6ec59e78d67167d5fbd3844d747af2551449
YURESSA <goshrenko@gmail.com>
2025-11-11 19:04:37 +0500

    Рефакторинг импортов в admin_news.py: упорядочение импортов и удаление дублирующихся строк

--- backend/api/admin/admin_news.py
+++ backend/api/admin/admin_news.py
@@ -5,10 +5,11 @@ from flask import request
 from flask_jwt_extended import get_jwt_identity
 from flask_restx import Resource
 
+from backend.core.schemas.admin_schemas import create_parser, update_parser
+from backend.core.services.news_service.news_image_service import (get_photos_for_news,
+                                                                   add_photo_to_news, delete_photo_from_news)
 from . import admin_ns
 from .decorators import admin_required
-from backend.core.schemas.admin_schemas import create_parser, update_parser
-from backend.core.services.news_service.news_image_service import get_photos_for_news, add_photo_to_news, delete_photo_from_news
 from ...core.services.news_service.news_service import create_news_with_images, get_all_news, get_news_by_id, \
     update_news, delete_news
 
