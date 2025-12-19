9525e6b841cd7d5a1381c3a0fc7294f8e2307228
YURESSA <goshrenko@gmail.com>
2025-12-19 13:21:46 +0500

    Изменен docker-compose.yml

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -16,4 +16,4 @@ cd ..
 echo "Подготавливаю данные"
 python -m backend.app seed_reference_data
 
-exec gunicorn -w 2 -b 0.0.0.0:5000 backend.wsgi:app --timeout 90
+exec gunicorn -w 3 -b 0.0.0.0:5000 backend.wsgi:app --timeout 90
