c9729df7eec2303071966e44e49dc3a28b2f2bfb
Anton787 <antongagelganc6443@gmail.com>
2026-01-26 19:46:21 +0500

    fix merge

--- backend/api/frontend_logs/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_restx import Namespace
-
-frontend_logs_ns = Namespace(
-    'frontend-logs',
-    description='Логирование ошибок с фронтенда'
-)
-
-from . import frontend_logs  # noqa: F401, E402
